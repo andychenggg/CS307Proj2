954bb98dfad18417de21b3a26b94dc51feba83c6
AndyCheng <100212381+andychenggg@users.noreply.github.com>
2023-05-20 17:43:07 +0800

    fix some bugs

--- src/main/java/cse/cs307/databaseproj2/Interceptor/UserAccessInterceptor.java
+++ src/main/java/cse/cs307/databaseproj2/Interceptor/UserAccessInterceptor.java
@@ -4,6 +4,7 @@ import cse.cs307.databaseproj2.util.CookieManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,11 +13,15 @@ public class UserAccessInterceptor implements HandlerInterceptor {
     // if not, redirect to /login
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 防止它拦截预检
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())){
+            return true;
+        }
+
         if(CookieManager.checkWhetherValid(request, "loginId"))
             return true;
         else {
             System.err.println("Interceptor");
-            response.sendRedirect("/login");
             return false;
         }
     }
